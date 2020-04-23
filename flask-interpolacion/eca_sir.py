42790527a40acc08f5bbbfcdb5b1c5a9be48cb5b
carlos-zrm <carloszrm90@gmail.com>
2020-04-23 18:21:23 -0500

    Envio de datos

---
+++ flask-interpolacion/eca_sir.py
@@ -0,0 +1,169 @@
+from cairo import *
+from matplotlib.figure import Figure
+
+class ECA:
+    """
+        S_ij (healthy susceptible): float
+        I_ij infected
+        R_ij Recovered
+        N-ij Poblacion
+        Q(S,I,R)
+        DS= [100*S_ij]/100
+        s_ij = (DS, DI, DR)
+
+    """
+
+    def __init__(self , cell_x, cell_y, step, flask=None):
+        self.cell_x=cell_x
+        self.cell_y=cell_y
+        self.step = step
+        self.flask = flask
+        if flask == None:
+            self.gui = GUI(cell_x,cell_y,5)
+        #self.pob_act = np.full( (cell_x,cell_y,4), float)
+        #self.pob_sig = np.full( (cell_x,cell_y,4), float)
+        self.pob_act = np.empty(shape=[cell_x,cell_y,4 ])
+        self.pob_sig = np.empty(shape=[cell_x,cell_y,4 ])
+        self.epsilon = 0.4
+        self.v = 0.6
+        self.N = 100
+        self.m = 0.5
+        self.c = 1 
+        self.mu = self.c*self.m*self.v
+        self.vecindad=[(0,-1),(-1,0),(1,0),(0,1)]
+        self.total_s = []
+        self.total_i = []
+        self.total_r = []
+        
+        print("*")
+        self.initializate()
+        print("*Configuracion inicial creada*")
+    
+    def init_json(self, cell_x, cell_y, step ):
+        pass
+    def initializate (self):
+        i = 0 
+        j = 0
+        t_s = 0
+        while i < self.cell_x:
+            j = 0
+            while j < self.cell_y:
+                self.pob_act[i][j]=[1.,0., 0., self.N ]
+                self.pob_sig[i][j]=[1.,0., 0., self.N ]
+                t_s +=self.N
+                j = j +1
+            i = i +1
+        self.total_s.append(t_s)
+        self.total_i.append(0)
+        self.total_r.append(0)
+        self.pob_act[i//2][j//2]=[.7,0.3, 0., self.N ]
+        #self.pob_act[i//4][j//4]=[.7,0.3, 0., self.N ]
+    
+    def simulacion_flask(self):
+        self.evolucion_ti()
+        return self.pob_act
+
+
+    def simulation(self):
+        i = 0 
+        if self.flask == None:
+            self.gui.evolucion_sir(self.pob_act)
+        while i < self.step:
+            self.evolucion_ti()
+            if self.flask == None: 
+                self.gui.evolucion_sir(self.pob_act)
+            i = i +1
+        if self.flask == None:
+            self.gui.plot_sir(self.total_s ,self.total_i ,self.total_r  )
+    def evolucion_ti(self):
+
+        i = j = t_s = t_i = t_r = 0
+        while i < self.cell_x:
+            j = 0
+            while j < self.cell_y:
+                S = self.evoluvion_S(i,j)
+                I = self.evolucion_I(i,j)
+                R = self.evolucion_R(i,j)
+                t_s += int(S*self.N)
+                t_i += int(I*self.N)
+                t_r += int(R*self.N)
+                #print(S,I,R)
+                self.pob_sig[i][j]=[S,I,R,self.N]
+                
+                j = j+1
+            i = i + 1
+        aux = self.pob_act
+        self.pob_act = self.pob_sig
+        self.pob_sig = aux
+        
+        self.total_s.append(t_s)
+        self.total_i.append(t_i)
+        self.total_r.append(t_r)
+
+        
+
+    def evoluvion_S(self, i, j):
+        S = self.pob_act[i][j][0]
+        I = self.pob_act[i][j][1]
+        suma = self.suma(i,j)
+        s_1 = self.v*S*I
+        s_2 = S*suma
+        return S-s_1-s_2
+
+    def evolucion_I (self,i,j):
+        S = self.pob_act[i][j][0]
+        I = self.pob_act[i][j][1]
+        
+        suma = self.suma(i,j)
+        i_0 = (1-self.epsilon)*I 
+        i_1 = S*I*self.v
+        i_2 = S*suma
+        return i_0+i_1+i_2
+
+    def evolucion_R (self,i,j):
+        I = self.pob_act[i][j][1]
+        R = self.pob_act[i][j][2]
+
+        return R+self.epsilon*I
+    def suma(self, i, j):
+        Nd = self.pob_act[i][j][3]
+        r = 0.
+        """
+            -   (i,j-1)
+        (i-1,j) (i, j )   (i+1,j)
+                (i,j+1)
+        """
+        for v_ij in self.vecindad:
+            ii = i+ v_ij[0]
+            jj = j +v_ij[1]
+            if ii == -1:
+                ii = self.cell_x -1
+            if ii == self.cell_x :
+                ii = 0
+            if jj == -1 :
+                jj = self.cell_y - 1
+            if jj == self.cell_y : 
+                jj = 0
+            N = self.pob_act[ii][jj][3]/Nd
+            I = self.pob_act[ii][jj][1]
+            r = r + (  N *self.mu * I)
+        return r
+            
+    def graficas_flask(self):
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.plot(self.total_s , label = "Suceptibles")
+        axis.plot(self.total_i, label = "Infectados")
+        axis.plot(self.total_r, label = "Recuperados")
+        return fig 
+        """
+        plt.ylabel('Individuos')
+        plt.xlabel('Días')
+        plt.title(' Modelo SIR ')
+        plt.show()
+        """
+
+#eca = ECA (50,50,100)
+#eca.simulation()
+
+#solve { x - (ay^2 -2b y+c )/(ac - b^2) = 0 , y - ( r +  (x^.5)* (c-2rb + r^2 *a)^.5 ),  Constants -> {a,b,c} }
--- flask-interpolacion/run.py
+++ flask-interpolacion/run.py
@@ -2,11 +2,14 @@ from flask import Flask
 #from forms import PostForm
 import io
 
-from flask import render_template, request, redirect, url_for, Response
+from flask import render_template, request, redirect, url_for, Response, session
 import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from interpolacion import flask
+from eca_sir import *
+eca = None
 app = Flask(__name__ , template_folder='templates')
+app.secret_key = 'app secret key'
 app.debug = True
 
 @app.route("/")
@@ -35,7 +38,50 @@ def interpolacion_form():
     if form.validate_on_submit():
         numDatos= form.numDatos.dta
     """
+@app.route("/sir/", methods=["GET", "POST"])
+def sir():
+    if request.method == 'POST':
+        global eca    
+        eca = ECA (50,50,100,flask=True)
+        #session['eca'] = eca
+        if next:
 
+        #return redirect(next)
+            return redirect(url_for('sir_evolucion'))
+            #return Response(output.getvalue(), mimetype='image/png')
+        #return redirect(url_for('index'))
+        #return 'recurso no encontrado'
+        
+        #return redirect("/evolucion-sir", messages={"main":"Condition failed on page baz"})
+                # return Response(output.getvalue(), mimetype='image/png')
+    return render_template("parametros.html")
+
+@app.route("/evolucion-sir/", methods=["GET", "POST"])
+def sir_evolucion():
+    if request.method == 'GET':
+        #result = eca.simulacion_flask()
+        #render_template("sir_canvas.html", result=result)
+        
+        """
+        cell_x = request.form['cell_x']
+        print(cell_x)
+        """
+        #return redirect(url_for("sir_evolucion", result=result))
+        return render_template("sir_canvas.html")
+               
+    return render_template("sir_canvas.html")
+
+@app.route("/evolucion-sir-ajax/", methods=["GET"])
+def sir_evolucion_ajax():
+    
+    result = eca.simulacion_flask()
+    """
+        cell_x = request.form['cell_x']
+        print(cell_x)
+    """
+        #return redirect(url_for("sir_evolucion", result=result))
+    return render_template("sir_canvas_ajax.html", result=result)           
+    
 if __name__ == "__main__":
     app.run()
 
